f1b71dba9afdf296e04a35380718fec90da215ca
nabermi2020 <miki2017@ukr.net>
2019-08-30 19:42:40 +0300

    'Product grid' component is added

---
+++ src/app/dashboard/product-grid/product-grid.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/shared/servives/products.service';
+
+@Component({
+  selector: 'app-product-grid',
+  templateUrl: './product-grid.component.html',
+  styleUrls: ['./product-grid.component.scss']
+})
+export class ProductGridComponent implements OnInit {
+  products: any[];
+  constructor(private productsService: ProductService) { }
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.products = this.productsService.getProducts();
+  }
+
+}
