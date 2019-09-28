3ca9bbda59327b357443ba75eb9154510f5a46cf
nabermi2020 <miki2017@ukr.net>
2019-09-29 00:04:00 +0300

    UI for footer is done

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  author: string = 'M.Naberezhnyi';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  getCurrentYear() {
+    return new Date().getFullYear();
+  }
 }
