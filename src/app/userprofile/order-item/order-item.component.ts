7a10cc70d5ddf5dce70bc3a5db1999b9e68b64f4
nabermi2020 <miki2017@ukr.net>
2019-10-16 14:55:28 +0300

    Added possobility to navigate to Pdp page for appropriate product from 'Order-history' component

--- src/app/userprofile/order-item/order-item.component.ts
+++ src/app/userprofile/order-item/order-item.component.ts
@@ -1,5 +1,7 @@
+import { ProductService } from 'src/app/shared/services/products.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Order } from 'src/app/cart/order.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-item',
@@ -10,7 +12,8 @@ export class OrderItemComponent implements OnInit {
   @Input() order;
   
   isOrderDetailExpanded: boolean = false;
-  constructor() { }
+  constructor(private productsService: ProductService,
+              private router: Router) { }
 
   ngOnInit() { }
 
@@ -53,6 +56,11 @@ getProductsQuantity(item) {
  */
 toggleOrderDetail() {
   this.isOrderDetailExpanded = !this.isOrderDetailExpanded;
+}
+
+navigateToProductDetailPage(product) {
+  this.productsService.setSelectedProduct(product);
+  this.router.navigate([`dashboard/product-details/${product.id}`])
 }
 
 }
